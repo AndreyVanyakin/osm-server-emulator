29a6207f211e5a6df2548d6181c49fb42bf01af2
Vanyakin Andrey <vanyakin@opten.ru>
2019-07-26 13:03:29 +0300

    TCP ping endpoint added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const moment = require('moment');
+const express = require("express");
+const moment = require("moment");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-let sseExpress = require('sse-express');
-const init = require('./Init');
-const {multipleWireData, singleWireData, settings} = require('./DataFeed');
+let sseExpress = require("sse-express");
+const init = require("./Init");
+const { multipleWireData, singleWireData, settings } = require("./DataFeed");
 
 // To sync history with fresh
 let framesFromStart = 0;
@@ -16,70 +16,62 @@ let dataRateSecs = 10;
 let FRESHRATE = 10; // secs
 let HISTORYRATE = 300; //secs
 
-app.get('/sse', sseExpress, (req, res) => {
+app.get("/sse", sseExpress, (req, res) => {
+  console.log("Request from client");
 
-    console.log('Request from client');
+  //Emit init data
+  res.sse("schema", init);
 
-    //Emit init data
-    res.sse('schema', init);
-    
-    // Each 1 sec create a data slice signed with current time in UNIX, then feed it to SSE. Current TS serves as an ID
-    setInterval(() => {
-        let nowTS = moment().unix()
-        
-        res.sse('fresh', multipleWireData(nowTS, framesFromStart), nowTS);
-        framesFromStart = framesFromStart + FRESHRATE;
-    }, FRESHRATE * 1000);
+  // Each 1 sec create a data slice signed with current time in UNIX, then feed it to SSE. Current TS serves as an ID
+  setInterval(() => {
+    let nowTS = moment().unix();
 
+    res.sse("fresh", multipleWireData(nowTS, framesFromStart), nowTS);
+    framesFromStart = framesFromStart + FRESHRATE;
+  }, FRESHRATE * 1000);
 });
 
+//
+app.get("/", (req, res) => {
+  console.log("Incoming TCP ping");
+  res.send("Some response");
+});
 
+app.post("/h", (req, res) => {
+  let result = [];
+  let nowTS = moment().unix();
+  console.log(req.body);
 
-app.post('/h', (req, res) => {
+  req.body.forEach(req => {
+    // key = wire ID
+    let key = Object.keys(req).join("");
 
-    let result=[];
-    let nowTS = moment().unix();
-    console.log(req.body);
+    // Frame number for last fresh
+    const nowFrame = settings[key].startFrame + framesFromStart;
 
+    // get to individual array of ranges
+    req[key].forEach(arr => {
+      // loop through tss with step 1
+      for (let ts = arr[0]; ts <= arr[1]; ts = ts + HISTORYRATE) {
+        // console.log('ts', ts);
+        // console.log('now frame', nowFrame);
+        // console.log('now ts', nowTS);
 
-    req.body.forEach(req => {
-        
-        // key = wire ID
-        let key = Object.keys(req).join('');
-
-        // Frame number for last fresh
-        const nowFrame = settings[key].startFrame + framesFromStart;
-        
-
-        // get to individual array of ranges
-        req[key].forEach(arr => {
-            
-            // loop through tss with step 1
-            for (let ts=arr[0]; ts<=arr[1]; ts=ts+HISTORYRATE) {
-
-                
-                // console.log('ts', ts);
-                // console.log('now frame', nowFrame);
-                // console.log('now ts', nowTS);
-                
-                //FIXME: A bug, because of latency => have to round stuff
-                let frame = Math.round((nowFrame - (nowTS - ts)));
-                // console.log(
-                //     '[NOW FRAME]',nowFrame,
-                //     '[NOW TS]',nowTS,
-                //     '[CALC TS]',ts,
-                //     '[NOW TS] - [CALC TS]',(nowTS-ts),
-                //     '[DATA FRAME]',frame
-                // );
-                result.push(singleWireData(parseInt(key,10), frame, ts))
-
-            }
-        })
+        //FIXME: A bug, because of latency => have to round stuff
+        let frame = Math.round(nowFrame - (nowTS - ts));
+        // console.log(
+        //     '[NOW FRAME]',nowFrame,
+        //     '[NOW TS]',nowTS,
+        //     '[CALC TS]',ts,
+        //     '[NOW TS] - [CALC TS]',(nowTS-ts),
+        //     '[DATA FRAME]',frame
+        // );
+        result.push(singleWireData(parseInt(key, 10), frame, ts));
+      }
     });
+  });
 
-    res.send(result);
-
+  res.send(result);
 });
 
 app.listen(5000);
-
